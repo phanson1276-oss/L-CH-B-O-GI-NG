07d3ba305525de092cbd6606284445307efe825a
phanson1276-oss <phanson1276@gmail.com>
2026-09-06 19:48:33 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    Base: '/L-CH-B-O-GI-NG/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
